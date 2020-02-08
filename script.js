d664b31d9fa45229cdddb729d16db0fc74674397
Sanjula <sanjula8@gmail.com>
2020-02-08 13:22:12 -0600

    fixed the javascript to move to submit page

--- script.js
+++ script.js
@@ -6,109 +6,165 @@ var questionContainerEl = document.getElementById("question-container");
 var QuestionDisplay = document.getElementById("question-title");
 var questionEl = document.getElementById("question");
 var answerButtonsEl = document.getElementById("answer-buttons");
-var answer1 = document.getElementById("answer1");
-var answer2 = document.getElementById("answer2");
-var answer3 = document.getElementById("answer3");
-var answer4 = document.getElementById("answer4");
+var answer1 = document.getElementById("Answer1");
+var answer2 = document.getElementById("Answer2");
+var answer3 = document.getElementById("Answer3");
+var answer4 = document.getElementById("Answer4");
+var timerEl = document.getElementById("timer");
+var messageEl = document.getElementById("message");
+var submitBtn = document.getElementById("submit");
 
 let currentQuestionIndex;
 let userScore;
+var timerID;
 
 // Create Questions:
 var questions = [
 	{
-		question1: "What is Javascript?",
+		question: "What is Javascript?",
 		answers: [
-			{ AnswerA: "Correct", correct: true },
-			{ AnswerB: "Wrong", correct: false },
-			{ AnswerC: "Wrong", correct: false },
-			{ AnswerD: "Wrong", correct: false }
+			{ answer: "Correct", correct: true },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false }
 		]
 	},
 	{
-		question2: "What is this other thing?",
+		question: "What is this other thing?",
 		answers: [
-			{ AnswerA: "Wrong", correct: false },
-			{ AnswerB: "Wrong", correct: false },
-			{ AnswerC: "Correct", correct: true },
-			{ AnswerD: "Wrong", correct: false }
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Correct", correct: true },
+			{ answer: "Wrong", correct: false }
 		]
 	},
 	{
-		question3: "What is that thing over there?",
+		question: "What is that thing over there?",
 		answers: [
-			{ AnswerA: "Wrong", correct: false },
-			{ AnswerB: "Wrong", correct: false },
-			{ AnswerC: "Wrong", correct: false },
-			{ AnswerD: "Correct", correct: true }
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Correct", correct: true }
 		]
 	},
 	{
-		question4: "What is that thing right here?",
+		question: "What is that thing right here?",
 		answers: [
-			{ AnswerA: "Correct", correct: true },
-			{ AnswerB: "Wrong", correct: false },
-			{ AnswerC: "Wrong", correct: false },
-			{ AnswerD: "Wrong", correct: false }
+			{ answer: "Correct", correct: true },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false }
+		]
+	},
+	{
+		question: "dfdfdght here?",
+		answers: [
+			{ answer: "Correct", correct: true },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false },
+			{ answer: "Wrong", correct: false }
 		]
 	}
 ];
 
+var time = questions.length * 15;
 // Start Quiz
 startButton.addEventListener("click", startQuiz);
-nextButton.addEventListener("click", () => {
-	currentQuestionIndex++;
-	setNextQuestion();
-});
 
 function startQuiz() {
 	startButton.classList.add("hide");
 	currentQuestionIndex = 0;
 	userScore = 0;
+	timerID = setInterval(clockTick, 1000);
+
 	questionContainerEl.classList.remove("hide");
 	setNextQuestion();
 }
 
 // Starting Quiz will also start counter
-
+function clockTick() {
+	time--;
+	timerEl.textContent = time;
+	if (time <= 0) {
+		submitScore();
+	}
+}
 // Hitting start will display next Question and Answer buttons:
 
 function showQuestion() {
-	QuestionDisplay.textContent = questions[0].question1;
-	answer1.innerHTML = questions[0].answers[0].AnswerA;
-	answer2.innerHTML = questions[0].answers[1].AnswerB;
-	answer3.innerHTML = questions[0].answers[2].AnswerC;
-	answer4.innerHTML = questions[0].answers[3].AnswerD;
+	QuestionDisplay.textContent = questions[currentQuestionIndex].question;
+	answer1.innerHTML = questions[currentQuestionIndex].answers[0].answer;
+	answer2.innerHTML = questions[currentQuestionIndex].answers[1].answer;
+	answer3.innerHTML = questions[currentQuestionIndex].answers[2].answer;
+	answer4.innerHTML = questions[currentQuestionIndex].answers[3].answer;
 }
-
-// Show Question too!
 
 // Selecting correct answer will move to next question and display "correct"
 // Selecting correct answer will count towards user score
 
 function setNextQuestion() {
-	showQuestion(questions[currentQuestionIndex]);
+	showQuestion();
 }
 
-answerButtonsEl.addEventListener("click", function selectAnswer() {
-	for (let i = 0; i < questions.length; i++) {
-		const element = questions[i];
-		if (answers === true) {
-			setNextQuestion();
-			userScore++;
-		} else {
-			alert("wrong");
-		}
+answerButtonsEl.addEventListener("click", function selectAnswer(event) {
+	console.log(this);
+	console.log(event.target.getAttribute("data-index"));
+
+	var dataIndex = event.target.getAttribute("data-index");
+	// console.log(questions[currentQuestionIndex].answers[dataIndex]);
+	if (questions[currentQuestionIndex].answers[dataIndex].correct === true) {
+		userScore++;
+		messageEl.textContent = "Correct";
+		messageEl.setAttribute("style", "color:green");
+	} else if (
+		questions[currentQuestionIndex].answers[dataIndex].correct === false
+	) {
+		// Selecting wrong answer will subtract 15 seconds to user score
+		// Selecting wrong answer will not count towards user score
+		time -= 15;
+		timerEl.textContent = time;
+		messageEl.textContent = "Wrong";
+		messageEl.setAttribute("style", "color:red");
+	}
+
+	currentQuestionIndex++;
+	if (currentQuestionIndex === questions.length) {
+		submitScore();
+	} else {
+		setNextQuestion();
 	}
 });
 
 // Selecting wrong answer will display "Incorrect/Wrong" and continue to next question
-// Selecting wrong answer will not count towards user score
-// Selecting wrong answer will subtract 15 seconds to user score
+
+function submitScore() {
+	clearInterval(timerID);
+	var endScreen = document.getElementById("end-screen");
+	endScreen.classList.remove("hide");
+	var score = document.getElementById("score");
+	score.textContent = time;
+	var questionsEl = document.getElementById("question-container");
+
+	questionsEl.setAttribute("class", "hide");
+}
+submitBtn.onclick = saveHighScore;
 
 // After finishing all answers, user will be redirected to Submit Score Page
 // At last question, will redirect to submit score page
 
-// var lastQuestion = questions.length - 1;
-
 // User is able to save the score in local storage and able to display High Scores
+function saveHighScore() {
+	var initialsEl = document.getElementById("initials");
+	var initials = initialsEl.value;
+	if (initials !== "") {
+		var highScores =
+			JSON.parse(window.localStorage.getItem("highScores")) || [];
+		var newScore = {
+			score: time,
+			initials: initials
+		};
+		highScores.push(newScore);
+		window.localStorage.setItem("highScores", JSON.stringify(highScores));
+		window.location.href = "highScores.html";
+	}
+}
